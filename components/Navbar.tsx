a8bb1b4ce2c0c4ba6af918c2e5ee3c37ea3a81b5
kingkuta080 <alubaidullahi080@gmail.com>
2025-03-25 11:54:39 +0100

    pot commit removed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
-// pot
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [activeSection, setActiveSection] = useState("home");
