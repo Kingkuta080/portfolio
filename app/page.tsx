2ab46add3a5b9bc3b336d245cb083f7f3f719aff
kingkuta080 <alubaidullahi080@gmail.com>
2025-03-25 11:15:37 +0100

    image comment

--- app/page.tsx
+++ app/page.tsx
@@ -97,6 +97,7 @@ export default function Home() {
             transition={{ duration: 0.8 }}
             className="md:w-1/2"
           >
+            {/* image */}
             <Image
               src={bg}
               alt="Ubaidullah Abdullah"
